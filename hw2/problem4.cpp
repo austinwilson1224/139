993c172288842617df2b9b00eb799b195a718efb
austinwilson1224 <austin1224@gmail.com>
2020-10-04 08:19:19 -0700

    hw2

--- hw2/problem4.cpp
+++ hw2/problem4.cpp
@@ -5,21 +5,15 @@
  * g++ -o problem4 problem4.cpp
  * ./a
  */
-
-
-using namespace std;
 #include <iostream>
-#include <ctime> /* clock_t, clock, CLOCKS_PER_SEC */
-
-
-
+#include <ctime>
+using namespace std;
 int main() {
-
     clock_t t = clock();
     if (t > 3000) {
         return 0;
     }
-    cout << t << "\n" ;
+    cout << t << " ";
     main();
     return 0;
 }
--- hw2/system1.cpp
+++ hw2/system1.cpp
@@ -1,38 +1,18 @@
 /**
- * g++ -o system2 system2.cpp
- * ./system2
+ * g++ -o system1 system1.cpp
+ * ./system1
  */
-
-using namespace std;
-
 #include <iostream>
-#include <string>
-
+//using namespace std;
 int main(int argc, char *argv[]) {
-
-    string arguments[] = {"ls","pwd","whoami"};
-
-    cout << "next system command:" << endl;
+    //cout << "next system command:" << endl;
     system("ls");
-    cout << "\n";
-
-    cout << "next system command:" << endl;
+    //cout << "\n";
+    //cout << "next system command:" << endl;
     system("pwd");
-    cout << "\n";
-
-    cout << "next system command:" << endl;
+    //cout << "\n";
+    //cout << "next system command:" << endl;
     system("whoami");
-    cout << "\n";
-
-
-
-    // cout << "\n\n\nOther way:\n";
-
-    // int arraySize = sizeof(arguments) / sizeof(arguments[0]);
-
-    // for(int i = 0; i < arraySize; i++) {
-    //     string temp = arguments[i];
-    //     cout << system(temp.c_str()) << endl;
-    // }
+    //cout << "\n";
     return 0;
 }
