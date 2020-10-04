2526328302226cfcc935fc530581d94bf83cdeb2
austinwilson1224 <austin1224@gmail.com>
2020-10-04 14:13:28 -0700

    no changes really

--- hw2/problem4.cpp
+++ hw2/problem4.cpp
@@ -8,12 +8,23 @@
 #include <iostream>
 #include <ctime>
 using namespace std;
-int main() {
-    clock_t t = clock();
-    if (t > 3000) {
+
+int i = 0;
+int main(int argc, char *argv[]) {
+    // clock_t t = clock();
+    // if (t > 3000) {
+    //     return 0;
+    // }
+    // cout << t << " ";
+    // main();
+    // return 0;
+    // cout << argc << "\n\n";
+
+    if(argc > 10) {
         return 0;
     }
-    cout << t << " ";
-    main();
-    return 0;
+
+    std::cout << i;
+    main(i++, argv);
 }
+
