1edee578fc819281d104e538bd9f0440910e82ab
austinwilson1224 <austin1224@gmail.com>
2020-10-15 15:06:36 -0700

    stuff

---
+++ hw3/vmm2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+# define MAX 4
+
+int main(int argc, char ** argv) {
+
+
+    std::cout << "Hello, world!\n";
+    return 0;
+}
+
+typedef int mat_tp[MAX][MAX];
+mat_tp a,b,c;
+void mat_mult(mat_tp m) {
+    for(int row = 0; row < MAX; row++) {
+        for(int col; col < MAX; col++) {
+            
+        }
+    }
+}
