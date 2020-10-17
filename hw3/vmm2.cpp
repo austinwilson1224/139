c7a54b0a57e7218e4119a01bc0e8cb1be83583b0
austinwilson1224 <austin1224@gmail.com>
2020-10-16 19:51:43 -0700

    hw3

--- hw3/vmm2.cpp
+++ hw3/vmm2.cpp
@@ -2,6 +2,9 @@
 
 # define MAX 4
 
+typedef int mat_tp[MAX][MAX];
+mat_tp a,b,c;
+
 int main(int argc, char ** argv) {
 
 
@@ -9,12 +12,24 @@ int main(int argc, char ** argv) {
     return 0;
 }
 
-typedef int mat_tp[MAX][MAX];
-mat_tp a,b,c;
-void mat_mult(mat_tp m) {
+
+void mat_mult(mat_tp a, mat_tp b, mat_tp m) {
     for(int row = 0; row < MAX; row++) {
         for(int col; col < MAX; col++) {
-            
+            m[row][col] = 0;
+            for(int i  = 0; i < MAX; i++) {
+                m[row][col] += a[row][i] * b[i][col];
+            }
+        }
+    }
+}
+
+
+void init() {
+    for(int col = 0; col < MAX; col++) {
+        for(int row = 0; row < MAX; row++) {
+            a[row][col] = 0;
+            b[row][col] = 0;
         }
     }
 }
